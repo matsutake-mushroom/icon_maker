1bdb2e162f8b7c8e26645a3b81de0eb6d087470c
T Matsutake <t.matsutake@hioki.co.jp>
2020-05-22 16:48:21 +0900

    initial

---
+++ アイコン作成くん/Form1.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace アイコン作成くん
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox16 = new System.Windows.Forms.CheckBox();
+            this.checkBox32 = new System.Windows.Forms.CheckBox();
+            this.checkBox48 = new System.Windows.Forms.CheckBox();
+            this.checkBox64 = new System.Windows.Forms.CheckBox();
+            this.checkBox128 = new System.Windows.Forms.CheckBox();
+            this.checkBox256 = new System.Windows.Forms.CheckBox();
+            this.checkBox0 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label_size = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.PictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.PictureBox1_DragEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("UD デジタル 教科書体 NP-B", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "１．画像ファイルを選択";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("UD デジタル 教科書体 NP-B", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(322, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(257, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "２．格納したいサイズを選ぶ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("UD デジタル 教科書体 NP-B", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(12, 302);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(203, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "またはファイルパスから指定";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.button1.Font = new System.Drawing.Font("UD デジタル 教科書体 NP-B", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(326, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "変　換";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(238, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(29, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(15, 322);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // checkBox16
+            // 
+            this.checkBox16.AutoSize = true;
+            this.checkBox16.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox16.Location = new System.Drawing.Point(348, 40);
+            this.checkBox16.Name = "checkBox16";
+            this.checkBox16.Size = new System.Drawing.Size(83, 27);
+            this.checkBox16.TabIndex = 5;
+            this.checkBox16.Text = "16 x 16";
+            this.checkBox16.UseVisualStyleBackColor = true;
+            // 
+            // checkBox32
+            // 
+            this.checkBox32.AutoSize = true;
+            this.checkBox32.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox32.Location = new System.Drawing.Point(348, 73);
+            this.checkBox32.Name = "checkBox32";
+            this.checkBox32.Size = new System.Drawing.Size(83, 27);
+            this.checkBox32.TabIndex = 5;
+            this.checkBox32.Text = "32 x 32";
+            this.checkBox32.UseVisualStyleBackColor = true;
+            // 
+            // checkBox48
+            // 
+            this.checkBox48.AutoSize = true;
+            this.checkBox48.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox48.Location = new System.Drawing.Point(348, 106);
+            this.checkBox48.Name = "checkBox48";
+            this.checkBox48.Size = new System.Drawing.Size(83, 27);
+            this.checkBox48.TabIndex = 5;
+            this.checkBox48.Text = "48 x 48";
+            this.checkBox48.UseVisualStyleBackColor = true;
+            // 
+            // checkBox64
+            // 
+            this.checkBox64.AutoSize = true;
+            this.checkBox64.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox64.Location = new System.Drawing.Point(348, 139);
+            this.checkBox64.Name = "checkBox64";
+            this.checkBox64.Size = new System.Drawing.Size(83, 27);
+            this.checkBox64.TabIndex = 5;
+            this.checkBox64.Text = "64 x 64";
+            this.checkBox64.UseVisualStyleBackColor = true;
+            // 
+            // checkBox128
+            // 
+            this.checkBox128.AutoSize = true;
+            this.checkBox128.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox128.Location = new System.Drawing.Point(348, 172);
+            this.checkBox128.Name = "checkBox128";
+            this.checkBox128.Size = new System.Drawing.Size(101, 27);
+            this.checkBox128.TabIndex = 5;
+            this.checkBox128.Text = "128 x 128";
+            this.checkBox128.UseVisualStyleBackColor = true;
+            // 
+            // checkBox256
+            // 
+            this.checkBox256.AutoSize = true;
+            this.checkBox256.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox256.Location = new System.Drawing.Point(348, 205);
+            this.checkBox256.Name = "checkBox256";
+            this.checkBox256.Size = new System.Drawing.Size(101, 27);
+            this.checkBox256.TabIndex = 5;
+            this.checkBox256.Text = "256 x 256";
+            this.checkBox256.UseVisualStyleBackColor = true;
+            // 
+            // checkBox0
+            // 
+            this.checkBox0.AutoSize = true;
+            this.checkBox0.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.checkBox0.Location = new System.Drawing.Point(348, 238);
+            this.checkBox0.Name = "checkBox0";
+            this.checkBox0.Size = new System.Drawing.Size(119, 27);
+            this.checkBox0.TabIndex = 5;
+            this.checkBox0.Text = "自分で決める";
+            this.checkBox0.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.textBox2.Location = new System.Drawing.Point(473, 236);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 30);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "0";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.textBox2.TextChanged += new System.EventHandler(this.TextBox2_TextChanged);
+            // 
+            // label_size
+            // 
+            this.label_size.AutoSize = true;
+            this.label_size.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label_size.Location = new System.Drawing.Point(527, 239);
+            this.label_size.Name = "label_size";
+            this.label_size.Size = new System.Drawing.Size(32, 23);
+            this.label_size.TabIndex = 7;
+            this.label_size.Text = "x 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(437, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "タスクトレイ等";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(437, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 12);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "小アイコン";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(437, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 12);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "タスクバー";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(437, 146);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 12);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "中アイコン";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(455, 179);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 12);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "タイル";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(455, 212);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 12);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "最大サイズ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(495, 80);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 12);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "一覧・詳細表示";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(495, 146);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 12);
+            this.label11.TabIndex = 8;
+            this.label11.Text = "並べて表示";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 355);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label_size);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.checkBox0);
+            this.Controls.Add(this.checkBox256);
+            this.Controls.Add(this.checkBox128);
+            this.Controls.Add(this.checkBox64);
+            this.Controls.Add(this.checkBox48);
+            this.Controls.Add(this.checkBox32);
+            this.Controls.Add(this.checkBox16);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "アイコン作成くん";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox16;
+        private System.Windows.Forms.CheckBox checkBox32;
+        private System.Windows.Forms.CheckBox checkBox48;
+        private System.Windows.Forms.CheckBox checkBox64;
+        private System.Windows.Forms.CheckBox checkBox128;
+        private System.Windows.Forms.CheckBox checkBox256;
+        private System.Windows.Forms.CheckBox checkBox0;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label_size;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ アイコン作成くん/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace アイコン作成くん
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.AllowDrop = true;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+        }
+
+        private void PictureBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileNames = (string[])e.Data.GetData(DataFormats.FileDrop);
+            string fileName = fileNames[0];
+            pictureBox1.ImageLocation = fileName;
+            textBox1.Text = fileName;
+        }
+
+        private void PictureBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                int size = Convert.ToInt32(textBox2.Text);
+                label_size.Text = "x " + size.ToString();
+
+                if (size <= 0 || size > 256)
+                {
+                    checkBox0.Checked = false;
+                    checkBox0.Enabled = false;
+                    throw new Exception("サイズは1～256で指定してください。");
+                }
+
+                checkBox0.Enabled = true;
+
+            }
+            catch (Exception ee)
+            {
+                checkBox0.Checked = false;
+                checkBox0.Enabled = false;
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Image img = null;
+            try
+            {
+                img = Image.FromFile(pictureBox1.ImageLocation);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("ファイルを開けませんでした。\n\n" + exc.Message, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            bool check = false;
+
+            var data_array = new List<IconData>();
+
+            if (checkBox16.Checked)
+            {
+                data_array.Add(new IconData(16,img));
+                check = true;
+            }
+            if (checkBox32.Checked)
+            {
+                data_array.Add(new IconData(32,img));
+                check = true;
+            }
+            if (checkBox48.Checked)
+            {
+                data_array.Add(new IconData(48,img));
+                check = true;
+            }
+            if (checkBox64.Checked)
+            {
+                data_array.Add(new IconData(64,img));
+                check = true;
+            }
+            if (checkBox128.Checked)
+            {
+                data_array.Add(new IconData(128,img));
+                check = true;
+            }
+            if (checkBox256.Checked)
+            {
+                data_array.Add(new IconData(256,img));
+                check = true;
+            }
+            if (checkBox0.Checked)
+            {
+                data_array.Add(new IconData(Convert.ToInt32(textBox2.Text),img));
+                check = true;
+            }
+
+            if (!check)
+            {
+                MessageBox.Show("サイズを選択してください。");
+                return;
+            }
+            //データ
+            List<byte> ret = new List<byte>();
+
+            //ヘッダ作成
+
+            byte[] header = new byte[6];
+            header[0] = 0x00;//reserved
+            header[1] = 0x00;//
+            header[2] = 0x01;//ICOファイル指定
+            header[3] = 0x00;//
+            int n_img = data_array.Count;
+            header[4] = (byte) (n_img);
+            header[5] = (byte) (n_img >> 8);
+
+            ret.AddRange(header);
+
+            //ディレクトリヘッダ作成
+            uint counter = (uint)header.Length;
+            counter += (uint)(16 * data_array.Count);//ヘッダ長さ固定＊個数
+
+            foreach (var icon in data_array)
+            {
+                icon.setDataOffset(counter);//ディレクトリヘッダを完成させる
+                ret.AddRange(icon.header);//ヘッダを書き込む
+                counter += (uint)icon.img_data.Length;
+            }
+
+            //データ部作成
+            foreach (var icon in data_array)
+            {
+                ret.AddRange(icon.img_data);
+            }
+
+            var dir = Path.GetDirectoryName(pictureBox1.ImageLocation);
+            var fname = Path.GetFileNameWithoutExtension(pictureBox1.ImageLocation);
+            var filename = Path.Combine(dir, fname + ".ico");
+
+            using (var fd = new SaveFileDialog())
+            {
+                fd.RestoreDirectory = true;
+                fd.InitialDirectory = dir;
+                fd.Title = "保存先・保存ファイル名を選んでください。";
+                fd.FileName = fname + ".ico";
+                    fd.Filter = "Iconファイル(*.ico)|*.ico|すべてのファイル(*.*)|*.*";
+
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    filename = fd.FileName;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            
+
+            
+
+            try
+            {
+                File.WriteAllBytes(filename, ret.ToArray());
+                MessageBox.Show("アイコンファイル\n" + filename + "\nを作成しました。");
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "エラー");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            using (var fd = new OpenFileDialog())
+            {
+                fd.CheckFileExists = true;
+                fd.Title = "画像ファイルを選択してください。";
+
+                if (fd.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.ImageLocation = fd.FileName;
+                }
+            }
+        }
+    }
+
+
+    public class IconData
+    {
+        private int width;
+        private int height;
+        public byte[] header;
+        public byte[] img_data;
+
+        public IconData(int size, Image src)
+        {
+            width = size;
+            height = size;
+
+            header = new byte[16];
+
+            header[0] = (byte) width;
+            header[1] = (byte) height;
+
+            header[2] = 0x00;//カラーパレットを使わない
+            header[3] = 0x00;//reserved
+
+            header[4] = 0x01;//ICOフォーマット
+            header[5] = 0x00;//
+
+            header[6] = 0x00;//PNGを使うのでbit深度は指定しない
+            header[7] = 0x00;//
+
+            using (Bitmap bitmap = new Bitmap(src, width, height)) {
+                using (var stream = new MemoryStream())
+                {
+                    bitmap.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                    img_data = stream.ToArray();
+                }
+            }
+
+            uint n_bytes_img = (uint)img_data.Length;
+            header[8] = (byte) n_bytes_img;
+            header[9] = (byte) (n_bytes_img >> 8);
+            header[10] = (byte)(n_bytes_img >> 16);
+            header[11] = (byte)(n_bytes_img >> 24);
+
+        }
+
+        public void setDataOffset(uint num)
+        {
+            header[12] = (byte)num;
+            header[13] = (byte)(num >> 8);
+            header[14] = (byte)(num >> 16);
+            header[15] = (byte)(num >> 24);
+        }
+
+
+    }
+}
